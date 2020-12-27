6c753a75625fa4777c26ed2a2fdead93e6cf9de9
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-12-27 16:09:54 -0300

    Cherry-pick improvements from beer

--- dev/settings.py
+++ dev/settings.py
@@ -46,13 +46,13 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['localhost', '127.0.0.1', '[::1]'])
 
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
 ]
 
 MIDDLEWARE = [
